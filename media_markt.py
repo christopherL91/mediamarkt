b1439fe2a683e9033f1f7aa232833f16578c3989
Christopher Lillthors <christopher.lillthors@gmail.com>
2015-11-11 18:28:36 +0100

    Added support for argument flags

--- media_markt.py
+++ media_markt.py
@@ -7,41 +7,49 @@ import os
 import heapq
 from sys import exit
 from termcolor import colored
+import argparse
 
 ID = int(os.environ["CUSTOMER_ID"])
 
-if ID == "":
-    exit('Unknown customer id')
+def getStatus(verbose=False):
+    serviceURL = 'http://www.serviceconnection.nu/MediaMarkt/servicestatus/View/OrderSummary.aspx?orderRecId={:d}'.format(ID)
 
-serviceURL = 'http://www.serviceconnection.nu/MediaMarkt/servicestatus/View/OrderSummary.aspx?orderRecId={:d}'.format(ID)
+    logURL = 'http://www.serviceconnection.nu/MediaMarkt/servicestatus/View/EventLog.aspx?orderRecId={:d}'.format(ID)
 
-logURL = 'http://www.serviceconnection.nu/MediaMarkt/servicestatus/View/EventLog.aspx?orderRecId={:d}'.format(ID)
+    serviceRequest = requests.get(serviceURL)
 
-serviceRequest = requests.get(serviceURL)
+    soup = BeautifulSoup(serviceRequest.text,"html.parser")
 
-soup = BeautifulSoup(serviceRequest.text,"html.parser")
+    status = soup.find(id="ctl00_Body_OrderstatusLabel").text
+    date = soup.find(id="ctl00_Body_OrderdateLabel").text
 
-status = soup.find(id="ctl00_Body_OrderstatusLabel").text
-date = soup.find(id="ctl00_Body_OrderdateLabel").text
+    print("Current Status:", status)
+    print("Current arrival date:", date)
 
-print("Current Status:", status)
-print("Current arrival date:", date, end="\n\n")
+    if verbose:
+        logRequest = requests.get('http://www.serviceconnection.nu/MediaMarkt/servicestatus/View/EventLog.aspx?orderRecId=221257367')
 
-logRequest = requests.get('http://www.serviceconnection.nu/MediaMarkt/servicestatus/View/EventLog.aspx?orderRecId=221257367')
+        soup = BeautifulSoup(logRequest.text,"html.parser")
 
-soup = BeautifulSoup(logRequest.text,"html.parser")
+        logtable = soup.find(id="ctl00_Body_OrderEventsGridView").find_all('tr')
 
-logtable = soup.find(id="ctl00_Body_OrderEventsGridView").find_all('tr')
+        headers = logtable[0].find_all('th')
 
-headers = logtable[0].find_all('th')
+        tables = []
+        for i in range(1,len(logtable)):
+            data = logtable[i].find_all('td')
+            date = data[0].text
+            s = data[1].text
+            heapq.heappush(tables,(date,s))
 
-tables = []
-for i in range(1,len(logtable)):
-    data = logtable[i].find_all('td')
-    date = data[0].text
-    s = data[1].text
-    heapq.heappush(tables,(date,s))
+        tabletitle = colored('Log data','blue')
+        print("\n\n\t",tabletitle)
+        print(tabulate(tables,headers=[headers[0].text,headers[1].text]))
 
-tabletitle = colored('Log data','blue')
-print("\t",tabletitle)
-print(tabulate(tables,headers=[headers[0].text,headers[1].text]))
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v','--verbose',
+            help="Print log", action="store_true")
+    args = parser.parse_args()
+    getStatus(args.verbose)
+
